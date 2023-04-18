b9fd58aebbee9096ca4c2edb422956db0936a802
장선호 <sunho3456@naver.com>
2023-04-18 13:48:49 +0900

    test

---
+++ app/src/main/java/com/example/medi_nion_manage/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.medi_nion_manage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var a = 10
+        print(a)
+    }
+}
